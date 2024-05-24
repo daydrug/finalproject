cba012a2c027483efb99f3481aac2d4bfc76ce58
daydrug <zwerababua@gmail.com>
2024-05-24 16:34:52 +0200

    done

--- main.js
+++ main.js
@@ -1,53 +1,189 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const navLinks = document.querySelectorAll(".nav-link");
-    const sections = document.querySelectorAll("section");
+// // document.addEventListener("DOMContentLoaded", function () {
+// //     const navLinks = document.querySelectorAll(".nav-link");
+// //     const sections = document.querySelectorAll("section");
 
-    navLinks.forEach((link, index) => {
-        link.addEventListener("click", function(event) {
-            event.preventDefault();
+// //     navLinks.forEach((link, index) => {
+// //         link.addEventListener("click", function (event) {
+// //             event.preventDefault();
 
-            navLinks.forEach(link => {
-                link.classList.remove("active");
-            });
+// //             navLinks.forEach(link => {
+// //                 link.classList.remove("active");
+// //             });
 
-            this.classList.add("active");
+// //             this.classList.add("active");
 
-            sections[index].scrollIntoView({
-                behavior: "smooth" });
-        });
-    });
+// //             sections[index].scrollIntoView({
+// //                 behavior: "smooth"
+// //             });
+// //         });
+// //     });
+// // });
+
+document.querySelector(".mobile-button").addEventListener("click", function(event)
+{
+    document.querySelector(".mobile-menu").classList.toggle("hide");
+    document.querySelectorAll(".mobile-btn").forEach(function(item) {
+      item.classList.toggle("btn-hide");
+    })
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    let menu = document.querySelector(".fixed-menu");
+let menu = document.querySelector(".fixed-menu");
+menu.addEventListener('click', function(event){
+    let clickTarget = event.target;
+    let activeBtn = document.querySelector('.active');
+    if (clickTarget.classList.contains('nav-link') ){
+        clickTarget.classList.add('active');
+        activeBtn.classList.remove('active');
 
-    menu.addEventListener('click', function(event) {
-        let clickTarget = event.target;
-        let activeBtn = document.querySelector('.active');
-        if (clickTarget.classList.contains('nav-link')) {
-            clickTarget.classList.add('active');
-            activeBtn.classList.remove('active');
+        let mobile = document.querySelector('.mobile-menu');
+        if (!mobile.classList.contains('hide')){
+            mobile.classList.add('hide');
         }
-    });
-
-    window.onscroll = function() {
-        let h = document.documentElement.clientHeight;
-        let classLink = '.main-link';
-        if (window.scrollY >= h * 4) {
-            classLink = '.comments-link';
-        } else if (window.scrollY >= h * 3) {
-            classLink = '.works-link';
-        } else if (window.scrollY >= h * 2) {
-            classLink = '.skills-link';
-        } else if (window.scrollY >= h) {
-            classLink = '.about-link';
-        }
-        let activeBtn = document.querySelector('.active');
-        let newActiveBtn = document.querySelector(classLink);
-        if (newActiveBtn && !newActiveBtn.classList.contains('active')) {
-            newActiveBtn.classList.add('active');
-            activeBtn.classList.remove('active');
-        }
-    };
+    }
+    let body = document.querySelector("body");
+    let mobileMenu = document.querySelector(".mobile-menu");
+    if (!body.classList.contains("off-scroll") && !mobileMenu.classList.contains('hide')){
+        body.classList.add("off-scroll");
+    }
+    else{
+      body.classList.remove("off-scroll");
+    }
 });
 
+let classLink = '.main-link';
+window.onscroll = function() {
+  let h = document.documentElement.clientHeight;
+  if (window.scrollY >= h4 ){
+    classLink = '.comments-link';
+  }
+  else if (window.scrollY >= h3){
+    classLink = '.works-link';
+  }
+  else if (window.scrollY >= h*2 ){
+    classLink = '.skills-link';
+  }
+  else if (window.scrollY >= h){
+    classLink = '.about-link';
+  }
+  else{
+    classLink = '.main-link';
+  }
+  let activeBtn = document.querySelector('.active');
+  let newActiveBtn = document.querySelector(classLink);
+  if (!newActiveBtn.classList.contains('active')){
+    newActiveBtn.classList.add('active');
+    activeBtn.classList.remove('active')
+  }
+};
+
+// document.querySelector(".mobile-button").addEventListener("click", function(event) {
+//     document.querySelector(".mobile-menu").classList.toggle("hide");
+// });
+
+// let menu = document.querySelector(".fixed-menu");
+// menu.addEventListener('click', function(event){
+//     let clickTarget = event.target;
+//     let activeBtn = document.querySelector('.active');
+//     if (clickTarget.classList.contains('nav-link')) {
+//         clickTarget.classList.add('active');
+//         activeBtn.classList.remove('active');
+
+//         let mobile = document.querySelector('.mobile-menu');
+//         if (!mobile.classList.contains('hide')) {
+//             mobile.classList.add('hide');
+//         }
+//     }
+//     let body = document.querySelector("body");
+//     let mobileMenu = document.querySelector(".mobile-menu");
+//     if (!body.classList.contains("off-scroll") && !mobileMenu.classList.contains('hide')) {
+//         body.classList.add("off-scroll");
+//     } else {
+//         body.classList.remove("off-scroll");
+//     }
+// });
+
+// let classLink = '.main-link';
+// window.onscroll = function() {
+//     let h = document.documentElement.clientHeight;
+//     if (window.scrollY >= h * 4) {
+//         classLink = '.comments-link';
+//     } else if (window.scrollY >= h * 3) {
+//         classLink = '.works-link';
+//     } else if (window.scrollY >= h * 2) {
+//         classLink = '.skills-link';
+//     } else if (window.scrollY >= h) {
+//         classLink = '.about-link';
+//     } else {
+//         classLink = '.main-link';
+//     }
+//     let activeBtn = document.querySelector('.active');
+//     let newActiveBtn = document.querySelector(classLink);
+//     if (newActiveBtn && !newActiveBtn.classList.contains('active')) {
+//         newActiveBtn.classList.add('active');
+//         activeBtn.classList.remove('active');
+//         let mobile = document.querySelector('.mobile-menu');
+//         if (!mobile.classList.contains('hide')) {
+//             mobile.classList.add('hide');
+//         }
+//     }
+// };
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     let menu = document.querySelector(".fixed-menu");
+
+//     menu.addEventListener('click', function (event) {
+//         let clickTarget = event.target;
+//         let activeBtn = document.querySelector('.active');
+//         if (clickTarget.classList.contains('nav-link')) {
+//             clickTarget.classList.add('active');
+//             activeBtn.classList.remove('active');
+//         }
+//         let body = document.querySelector("body");
+//         let mobileMenu = document.querySelector(".mobile-menu");
+//         if (!body.classList.contains("off-scroll") &&
+//             !mobileMenu.classList.contains("hide")) {
+//             body.classList.add("off-scroll");
+//         }
+//         else {
+//             body.classList.remove("off-scroll");
+//         }
+//     });
+
+//     window.onscroll = function () {
+//         let h = document.documentElement.clientHeight;
+//         let classLink = '.main-link';
+//         if (window.scrollY >= h * 4) {
+//             classLink = '.comments-link';
+//         } else if (window.scrollY >= h * 3) {
+//             classLink = '.works-link';
+//         } else if (window.scrollY >= h * 2) {
+//             classLink = '.skills-link';
+//         } else if (window.scrollY >= h) {
+//             classLink = '.about-link';
+//         }
+//         let activeBtn = document.querySelector('.active');
+//         let newActiveBtn = document.querySelector(classLink);
+//         if (newActiveBtn && !newActiveBtn.classList.contains('active')) {
+//             newActiveBtn.classList.add('active');
+//             activeBtn.classList.remove('active');
+//             let mobile = document.querySelector('.mobile-menu');
+//             if (!mobile.classList.contains('hide')) {
+//                 mobile.classList.add('hide');
+//             }
+//         }
+//     };
+// });
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     const mobileButton =
+//         document.querySelector(".mobile-button");
+//     const mobileMenu =
+//         document.querySelector(".mobile-menu");
+
+//     mobileButton.addEventListener("click", function () {
+//         mobileMenu.classList.toggle("hide");
+//     });
+// });
